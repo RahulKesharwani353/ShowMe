da8b6bea88eafcae0f79cef2a7a744e63b2bef5f
Akash Vekariya <akash.vekariya540@gmail.com>
2023-02-10 14:29:34 +0530

    Refined Models

--- ShowMe/Models/Theater.cs
+++ ShowMe/Models/Theater.cs
@@ -1,11 +1,8 @@
-﻿using System;
-namespace ShowMe.Models {
-	public class Theater {
-		public Guid Id { get; set; }
-		public string Name { get; set; }
-		public string Location { get; set; }
-		public DateTime CreatedOn { get; set; }
-		public DateTime UpdatedOn { get; set; }
-	}
+﻿namespace ShowMe.Models;
+public class Theater {
+	public Guid Id { get; set; }
+	public string Name { get; set; }
+	public string Location { get; set; }
+	public DateTime CreatedOn { get; set; }
+	public DateTime UpdatedOn { get; set; }
 }
-
